5fb934d6aef10922db297f8fe2e93ac069f0696f
Toan040606 <toannickson2k60604@gmail.com>
2025-07-14 19:44:04 +0700

    an ba to com

---
+++ Buoi2/src/Bai2.java
@@ -0,0 +1,10 @@
+public class Bai2 {
+    public static void main(String[] args) {
+        String studentName = "Trần Khánh Toàn";
+        int age = 19;
+        String classRoom = "2024DHKTPM03";
+        double GPA = 4.0;
+
+        System.out.println("Tên: " + studentName + " - Tuổi: " + age + " - Lớp: " + classRoom + " - GPA: " + GPA);
+    }
+}
